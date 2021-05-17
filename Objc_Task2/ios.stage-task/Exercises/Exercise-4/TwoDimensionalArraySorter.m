e3db3ccbb438cf3210e6ec4d7e94c67027df0a34
AnChugunov <justtobehappy123@gmail.com>
2021-05-18 02:58:13 +0300

    task 4

--- Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
@@ -3,7 +3,33 @@
 @implementation TwoDimensionalArraySorter
 
 - (NSArray *)twoDimensionalSort:(NSArray<NSArray *> *)array {
-    return @[];
-}
+    
+    if (array == nil || array.count == 0 || ![array[0] isKindOfClass:NSArray.class]) {
+            return @[];
+        }
+    NSArray *resultArr = [NSArray arrayWithObjects:arrOfDigits, arrOfStrings, nil];
+    NSMutableArray *arrOfStrings = [[NSMutableArray alloc] init];
+    NSMutableArray *arrOfDigits = [[NSMutableArray alloc] init];
+    for (int i = 0; i < array.count; i++) {
+        for (int j = 0; j < array[i].count; j++) {
+            if (([array[i][j] isKindOfClass:[NSString class]])) {
+                [arrOfStrings addObject:array[i][j]];
+            }
+            if ([array[i][j] isKindOfClass:[NSNumber class]]) {
+            [arrOfDigits addObject:array[i][j]];
+            }
+        }
+    }
+    if (arrOfStrings.count == 0) {
+        [arrOfDigits sortUsingDescriptors:[NSArray arrayWithObject:[NSSortDescriptor sortDescriptorWithKey:@"self" ascending:YES]]];
+        return arrOfDigits;
+    }
+    if (arrOfDigits.count == 0) {
+        return [arrOfStrings sortedArrayUsingSelector:@selector(localizedCaseInsensitiveCompare:)];
+    }
+    [arrOfDigits sortUsingDescriptors:[NSArray arrayWithObject:[NSSortDescriptor sortDescriptorWithKey:@"self" ascending:YES]]];
+    [arrOfStrings sortUsingDescriptors:[NSArray arrayWithObject:[NSSortDescriptor sortDescriptorWithKey:@"self" ascending:NO selector:@selector(localizedCaseInsensitiveCompare:)]]];
+    return resultArr;
+    }
 
 @end
