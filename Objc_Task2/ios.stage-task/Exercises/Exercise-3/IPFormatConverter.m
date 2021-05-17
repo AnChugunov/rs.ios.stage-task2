4d0295d1f238d9e3534a80643b01c2012242e7d1
AnChugunov <justtobehappy123@gmail.com>
2021-05-18 02:40:41 +0300

    added excs 1,3

--- Objc_Task2/ios.stage-task/Exercises/Exercise-3/IPFormatConverter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-3/IPFormatConverter.m
@@ -3,7 +3,21 @@
 @implementation IPFormatConverter
 
 - (NSString *)ipFormatConverter:(NSArray *)numbersArray {
-    return nil;
+    
+    NSMutableString *resultString = [NSMutableString new];
+    
+    if (numbersArray == nil || [numbersArray count] == 0) {
+           return @"";
+       }
+    for (NSNumber *number in numbersArray) {
+        int verificationValue = number.intValue;
+        if ((verificationValue > 255) || (verificationValue < 0) ) {
+            return @"Invalid value";
+        } else {
+            [result appendFormat:@"%d.", verificationValue];
+        }
+    }
+    return [resultSting substringToIndex:[resultString length] - 1];
 }
 
 @end
